2657d92e5355c0d70ae9e9d46fb25bcb2cd53059
Miguel Risco-Castillo <mriscoc@users.noreply.github.com>
2022-03-11 15:06:49 -0500

    🚸 Update Ender3 V2/S1 Pro UI (#23878)

--- Marlin/src/gcode/stats/M75-M78.cpp
+++ Marlin/src/gcode/stats/M75-M78.cpp
@@ -43,10 +43,10 @@
 void GcodeSuite::M75() {
   startOrResumeJob();
   #if ENABLED(DWIN_LCD_PROUI)
-    DWIN_Print_Header(parser.string_arg && parser.string_arg[0] ? parser.string_arg : GET_TEXT(MSG_HOST_START_PRINT));
     DWIN_Print_Started(false);
+    if (!IS_SD_PRINTING()) DWIN_Print_Header(parser.string_arg && parser.string_arg[0] ? parser.string_arg : GET_TEXT(MSG_HOST_START_PRINT));
   #elif ENABLED(DWIN_CREALITY_LCD_JYERSUI)
-    CrealityDWIN.Update_Print_Filename(parser.string_arg && parser.string_arg[0] ? parser.string_arg : GET_TEXT(MSG_HOST_START_PRINT));
+    if (!IS_SD_PRINTING()) CrealityDWIN.Update_Print_Filename(parser.string_arg && parser.string_arg[0] ? parser.string_arg : GET_TEXT(MSG_HOST_START_PRINT));
   #endif
 }
 
@@ -56,6 +56,7 @@ void GcodeSuite::M75() {
 void GcodeSuite::M76() {
   print_job_timer.pause();
   TERN_(HOST_PAUSE_M76, hostui.pause());
+  TERN_(DWIN_LCD_PROUI, DWIN_Print_Pause());
 }
 
 /**
